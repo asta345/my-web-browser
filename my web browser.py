ab3e0147e1670a9cd3ab9d6ff1a9f48982f5f1e0
asta345 <81383112+asta345@users.noreply.github.com>
2021-04-23 14:54:52 +0530

    Add files via upload

---
+++ my web browser.py
@@ -0,0 +1,19 @@
+Python Code From Task B1 :
+from http.server import HTTPServer, BaseHTTPRequestHandler
+class Serv(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/www.ingonline.nu/tictactoe/index.php?board=xoxoeoeex'
+        try:
+            file_to_open = open(self.path[1:]),read()
+            self.send_response(200)
+        except:
+            file_to_open = "File not found"
+            self.send_response(404)
+        self.end_headers()
+        self.wfile.write(bytes(file_to_open, 'utf-8'))
+
+
+httpd = HTTPServer(('localhost', 80), Serv)
+httpd.serve_forever()
